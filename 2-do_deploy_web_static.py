a1188317c912a0d142626b7f4054b97a89b9dbbf
livingstoneamisi <livingstoneamisi00@gmail.com>
2024-05-28 10:33:34 +0300

    2. Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import *
 from datetime import datetime
 import os
 
-env.hosts = ['54.144.136.64', '100.24.236.179']
+env.hosts = ['54.160.108.55', '100.26.164.108']
 env.user = 'ubuntu'
 
 
